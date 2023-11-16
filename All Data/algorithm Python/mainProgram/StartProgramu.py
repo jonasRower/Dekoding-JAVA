318b1b5b06db84e1f3b192462b2778c35f8b4a2f
jonasRower <119448235+jonasRower@users.noreply.github.com>
2023-11-16 14:31:24 +0100

    Delete All Data directory

--- All Data/algorithm Python/mainProgram/StartProgramu.py
+++
@@ -1,8 +0,0 @@
-import mainProgram.NactiZdroj
-import mainProgram.SeznamZdroju
-import mainProgram.NovyJAVAKod
-
-
-vykonavejHlavniProgram = mainProgram.SeznamZdroju.vykonavaniHlavnihoProgramu()
-vykonavejHlavniProgram.hlavniProgram()
-
